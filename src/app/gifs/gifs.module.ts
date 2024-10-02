8816572679e3cb5ef66f9404e7fc5ebb714ae8a0
Hugo <hoyola@ucv.edu.pe>
2024-10-02 17:33:03 -0500

    Agregando componente de lista de tarjetas de GIFs y página de inicio

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent, SearchComponent, CardListComponent],
+  imports: [CommonModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
